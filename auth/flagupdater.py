9c216dd609c59e35c7543eac0407669fa5b84254
Jangha Kim <james010kim@gmail.com>
2017-05-16 11:41:46 +0900

    Add json loader and pgp verifier

--- auth/flagupdater.py
+++ auth/flagupdater.py
@@ -1,7 +1,31 @@
-import socket, sys, re, json
+#!/usr/bin/python3
 
-def saveflag(jsondata):
-    print("saveflag")
+import socket, sys, re, json
+import gnupg
+import base64
+
+# Initialize GPG
+def initialize_gpg():
+    global gpg
+    homedir = '/home/vagrant/.gnupg'
+    try:
+        gpg = gnupg.GPG(gnupghome=homedir) 
+    except TypeError:
+        gpg = gnupg.GPG(homedir=homedir)
+
+def saveflag(recvdata):
+    jd = json.loads(recvdata)
+    print(jd['signer'])
+    print(jd['newflag'])
+    print(jd['signature'])
+
+    sign = base64.b64decode(jd['signature']);
+    data = jd['signer']+':'+jd['newflag'];
+
+    verified = gpg.verify_data(sign, data)
+
+    if not verified:
+        print("Invalid Update")
 
 def server():
     # Create a TCP/IP socket
@@ -9,8 +33,8 @@ def server():
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
     # Bind the socket to the port
-    server_address = ('localhost', 42)
-    print >> sys.stderr, 'starting up on %s port %s' %server_address
+    server_address = ('0.0.0.0', 42)
+    print('starting up on %s port %s' % server_address)
     sock.bind(server_address)
 
     # Listen for incoming connetions
@@ -18,19 +42,25 @@ def server():
     
     while True:
         connection, client_address = sock.accept()
+        print('accepting up on %s port %s' % client_address)
 
         try:
             data = []
             while True:
                 byte = connection.recv(1)
-                if byte == '':
+                if byte == b'' :
                     break
                 data.append(byte)
-            saveflag(''.join(data))
+            saveflag(b''.join(data).decode('utf-8'))
         finally:
             # Close the connection
             connection.close()
             print("closed")
 
 if __name__ == "__main__":
-    server()
+    initialize_gpg()
+    try:
+#        server()
+        saveflag("{\n\"signer\" : \"james010kim\", \"newflag\" : \"thisisnewflag\", \"signature\" : \"owGbwMvMwMHYExWo4SKzcxLjGrUk9tzidL2SipJISX3frMTc1GIDQ4PszFyrkozM4szivNTytJzE9E5GYxYGRg4GWTFFFt6yvNrTTUqrnvJmXoMZxMoE0s7AxSkAE0l6xcGwm6mxmls7LTivL9DI44r2cf+Lr/6aWc+5sNRwb3ULZ0x2xpKrlvqvX1z8tfjvCzlx/Tk6T2xS49wZZCP8rbq81s1aEeqgxWtzQ9jkjFM0j9/iG2kek27f3131UjZugewHEb5X5QEsoR/z/z/N/ZcoyF8Rss5TnfVc2MWA2x+/vfHXsd1Twc9ZUVamLtEYLn7FOfe6rrmEXchtJW8TPps9XjFyrg13XH9Mk95tHpQkqrNk6/4qb/0ZpcY7zizauDD1w8cyd1NrHhFul/DPUp+XCVn19E8N3/Y3PEQs15q9UHDOB1adjZ/PxWWHfTipZnsqe9u+89a1ng4XTjTrP/7dqrNB13+zhUGFlNt1AA==\" }")	# test for verification of newflag
+    except KeyboardInterrupt:
+        print ("Ctrl_C pressed ... Shutting Down")
