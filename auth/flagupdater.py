289c34fa36acfe21b78ac81c3dc7fb5dda87af63
Jangha Kim <james010kim@gmail.com>
2017-05-18 20:55:39 +0900

    Import TA's public keys

--- auth/flagupdater.py
+++ auth/flagupdater.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 
-import socket, sys, re, json
+import socket, sys, re, os
+import json
 import gnupg
 import base64
+
+TAPUBKEY = "tapubkey/"
 
 # Initialize GPG
 def initialize_gpg():
@@ -13,10 +16,29 @@ def initialize_gpg():
     except TypeError:
         gpg = gnupg.GPG(homedir=homedir)
 
+    # Import TA's key
+    fnames = os.listdir(TAPUBKEY)
+    for name in fnames:
+        with open(TAPUBKEY+name, "r") as f:
+            result = gpg.import_keys(f.read())
+            assert result
+
+def isTA(signer):
+    fnames = os.listdir(TAPUBKEY)
+    for name in fnames:
+        if os.path.splitext(name)[0] == signer:
+            return True
+    return False
+
 def saveflag(recvdata):
     jd = json.loads(recvdata)
 #    sign = base64.b64decode(jd['signature']);
     data = jd['signer']+":"+jd['newflag']
+
+    # Is signer TA
+    if not isTA(jd['signer']):
+        print("Not TA. kick!")
+        return False
 
     # if signature has data and sign,
 #    verified = gpg.verify("-----BEGIN PGP MESSAGE-----\n"+jd['signature'])		# signature has data and sign
@@ -31,6 +53,8 @@ def saveflag(recvdata):
 
     if not verified:
         return False
+
+    # Save flag to database
 
     return True
 
@@ -68,11 +92,11 @@ def server():
 if __name__ == "__main__":
     initialize_gpg()
     try:
-        server()
+#        server()
 
         # test for verification of newflag, 
         # signature has sign only.
-#        saveflag("{\n\"signer\" : \"james010kim\", \"newflag\" : \"thisisnewflag\", \"signature\" : \"iQEcBAEBAgAGBQJZGtwdAAoJEPCPYHY8FuWzfgoIAJ1/inUc4DqyY/20NmRW8UuQfOo/4Hv3a9gx4M8kYIrpEZJVowxvoVmuV8bEIlROHZZAlos2KniLk9yMunDSSoK+SpKg6fVET3l3q6O16kqQ6CIqFUch9B5bYxCNWPj9E65Cq8Spq7mhUmTbmeVvjNCXU/LZ7y4pzh8RX5oWSL3mkeiZTh+vB3h6AzdBcEdQHuSZDvctmsjAnLM1Xj2itoV3ZIebenFH3RXOew4CpmqiI8FP3rfuWG4x9XjXh9oxgG6ojImsJSxsMjT8efhpqYuvOF5YsxG5EsQK8nnkt2Lfs6Bxf01klwDfRpJENFF3R32C4fSUmsVKeARzgtFf1+U==rqA8\" }")	
+        saveflag("{\n\"signer\" : \"james010kim\", \"newflag\" : \"thisisnewflag\", \"signature\" : \"iQEcBAEBAgAGBQJZGtwdAAoJEPCPYHY8FuWzfgoIAJ1/inUc4DqyY/20NmRW8UuQfOo/4Hv3a9gx4M8kYIrpEZJVowxvoVmuV8bEIlROHZZAlos2KniLk9yMunDSSoK+SpKg6fVET3l3q6O16kqQ6CIqFUch9B5bYxCNWPj9E65Cq8Spq7mhUmTbmeVvjNCXU/LZ7y4pzh8RX5oWSL3mkeiZTh+vB3h6AzdBcEdQHuSZDvctmsjAnLM1Xj2itoV3ZIebenFH3RXOew4CpmqiI8FP3rfuWG4x9XjXh9oxgG6ojImsJSxsMjT8efhpqYuvOF5YsxG5EsQK8nnkt2Lfs6Bxf01klwDfRpJENFF3R32C4fSUmsVKeARzgtFf1+U==rqA8\" }")	
         # signature has data and sign.
 #        saveflag("{\n\"signer\" : \"james010kim\", \"newflag\" : \"thisisnewflag\", \"signature\" : \"owEBVgGp/pANAwACAfCPYHY8FuWzAawmYgdtc2cudHh0WR0C4GphbWVzMDEwa2ltOnRoaXNpc25ld2ZsYWeJARwEAAECAAYFAlkdAuAACgkQ8I9gdjwW5bPBEAgAsSGaZtJ03XxVcTgKlLBu+LRymtMRApc6I0I5e0H/9/5WDkLrPsUJ8G72fFu2oQ97sl/V9hUeT90xgqSHFgfZuxRanybTGbSNiJ1g56RY1B7Yszf+1kutgOZnab4xNAeadadzNOD2KYlrwfQFNVT2cVl4679oG17XRiQLJOIR+gAKIvjE0O/U+P8tQ6Yhe5mzhx9dbwawV7nUW9fvfQNQhPMR7F8Ek0wHkBwVfG2qbRjLuuAULA21BHHxQijyjpOdaTX9bhPURBQ20hHPfy1oWxwGHNbW17QoNV53tWHeL3UZ33g3/5iFOB1uuXGGTmpkOGd5wMC8b6RMR9qdC3lKjw==\" }")	
     except KeyboardInterrupt:
