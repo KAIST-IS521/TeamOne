e4528d577bc3e723c9752abff9f9d963622beb2b
Jiho Lee <cjdhlds08@gmail.com>
2017-05-18 15:38:49 +0900

    Fix reg_pubkey.py(bankDB ver1.1)

--- db/reg_pubkey.py
+++ db/reg_pubkey.py
@@ -1,7 +1,7 @@
 import pymysql
 import os
 
-# Get the key from ./pubkeys directory
+# Get the key from ./pubkeys directory - unnecessary func
 def get_pgp_key(github_id):
     pubkey_dir = "./pubkeys/"
     pubkey_file = pubkey_dir + github_id + ".pub"
@@ -13,7 +13,7 @@ def get_pgp_key(github_id):
     return pgp_key
 
 # Register github_id, pgp_key, reg_flag
-def register_info(github_id, pgp_key):
+def register_info(github_id):
     conn = pymysql.connect(host = 'localhost', 
                            user = 'root', 
                            password = 'DtWb3q2WcG0tc2fFgXi5sGx86XL7RT8g', 
@@ -21,10 +21,10 @@ def register_info(github_id, pgp_key):
                            charset = 'utf8')
     curs = conn.cursor()
 
-    query = "INSERT INTO auth_table(github_id, pgp_key, reg_flag) \
-                    VALUES (%s, %s, %s)"
+    query = "INSERT INTO auth_table(github_id, reg_flag) \
+                    VALUES (%s, %s)"
     try:
-        curs.execute(query, (github_id, pgp_key, 0))
+        curs.execute(query, (github_id, 0))
         conn.commit()
     except:
         print ("Can't register github_id: %s" % github_id)
@@ -34,18 +34,18 @@ def register_info(github_id, pgp_key):
 
 if __name__ == "__main__":
     id_list_fp = open("github_id.list", "r")
-    pubkey_dir = "./pubkeys/"
 
     while True:
         github_id = id_list_fp.readline()
         if not github_id: break
         github_id = github_id[:-1]
         print (github_id)
+        register_info(github_id)
 
-        pgp_key = get_pgp_key(github_id)
+        '''pgp_key = get_pgp_key(github_id)
         if pgp_key is None:
             print ("There is no pubkey file.")
         else:
-            register_info(github_id, pgp_key)
+            register_info(github_id, pgp_key)'''
 
     id_list_fp.close()
