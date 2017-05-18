fb532533c2c3738daee64a682591e8a2521500de
Jangha Kim <james010kim@gmail.com>
2017-05-18 20:09:55 +0900

    Signature verification done

--- auth/flagupdater.py
+++ auth/flagupdater.py
@@ -15,26 +15,24 @@ def initialize_gpg():
 
 def saveflag(recvdata):
     jd = json.loads(recvdata)
-#    print(jd['signer'])
-#    print(jd['newflag'])
-#    print(jd['signature'])
+#    sign = base64.b64decode(jd['signature']);
+    data = jd['signer']+":"+jd['newflag']
 
-    sign = base64.b64decode(jd['signature']);
-    data = jd['signer']+':'+jd['newflag'];
+    # if signature has data and sign,
+#    verified = gpg.verify("-----BEGIN PGP MESSAGE-----\n"+jd['signature'])		# signature has data and sign
 
-    with open("/home/vagrant/shared/teamone/msg.txt.sig.det", mode='rb') as file: # b is important -> binary
-        fileContent = file.read()
-
-#    fileContent = gpg.sign(data, default_key='TeamOne@hank.com', passphrase='qwer1234', clearsign=False)
-#    print(data)
-#    print(fileContent)
-#    verified = gpg.verify(fileContent)
-#    verified = gpg.verify_file("/home/vagrant/shared/teamone/msg.txt.sig.det", sig_file="/home/vagrant/shared/teamone/msg.txt")
-#    verified = gpg.verify_file("/home/vagrant/shared/teamone/msg.txt", sig_file="/home/vagrant/shared/teamone/msg.txt.sig.det")
-    verified = gpg.verify_data("/home/vagrant/shared/teamone/msg.txt.sig.det", data)
+    # signature has sign only. build an armor for gnupg
+    pgparmor  = "-----BEGIN PGP SIGNED MESSAGE-----"+"\n"+"Hash: SHA1"+"\n\n"   
+    pgparmor += data + "\n"
+    pgparmor += "-----BEGIN PGP SIGNATURE-----"+"\n"+"Version: GnuPG v1"+"\n\n"
+    pgparmor += jd['signature'] + "\n"
+    pgparmor += "-----END PGP SIGNATURE-----"+"\n"
+    verified = gpg.verify(pgparmor)
 
     if not verified:
-        print("Invalid Update")
+        return False
+
+    return True
 
 def server():
     # Create a TCP/IP socket
@@ -60,7 +58,8 @@ def server():
                 if byte == b'' :
                     break
                 data.append(byte)
-            saveflag(b''.join(data).decode('utf-8'))
+            if not saveflag(b''.join(data).decode('utf-8')) :
+                print ("invalid update message")
         finally:
             # Close the connection
             connection.close()
@@ -69,7 +68,12 @@ def server():
 if __name__ == "__main__":
     initialize_gpg()
     try:
-#        server()
-        saveflag("{\n\"signer\" : \"james010kim\", \"newflag\" : \"thisisnewflag\", \"signature\" : \"owGbwMvMwMHYExWo4SKzcxLjGrUk9tzidL2SipJISX3frMTc1GIDQ4PszFyrkozM4szivNTytJzE9E5GYxYGRg4GWTFFFt6yvNrTTUqrnvJmXoMZxMoE0s7AxSkAE0l6xcGwm6mxmls7LTivL9DI44r2cf+Lr/6aWc+5sNRwb3ULZ0x2xpKrlvqvX1z8tfjvCzlx/Tk6T2xS49wZZCP8rbq81s1aEeqgxWtzQ9jkjFM0j9/iG2kek27f3131UjZugewHEb5X5QEsoR/z/z/N/ZcoyF8Rss5TnfVc2MWA2x+/vfHXsd1Twc9ZUVamLtEYLn7FOfe6rrmEXchtJW8TPps9XjFyrg13XH9Mk95tHpQkqrNk6/4qb/0ZpcY7zizauDD1w8cyd1NrHhFul/DPUp+XCVn19E8N3/Y3PEQs15q9UHDOB1adjZ/PxWWHfTipZnsqe9u+89a1ng4XTjTrP/7dqrNB13+zhUGFlNt1AA==\" }")	# test for verification of newflag
+        server()
+
+        # test for verification of newflag, 
+        # signature has sign only.
+#        saveflag("{\n\"signer\" : \"james010kim\", \"newflag\" : \"thisisnewflag\", \"signature\" : \"iQEcBAEBAgAGBQJZGtwdAAoJEPCPYHY8FuWzfgoIAJ1/inUc4DqyY/20NmRW8UuQfOo/4Hv3a9gx4M8kYIrpEZJVowxvoVmuV8bEIlROHZZAlos2KniLk9yMunDSSoK+SpKg6fVET3l3q6O16kqQ6CIqFUch9B5bYxCNWPj9E65Cq8Spq7mhUmTbmeVvjNCXU/LZ7y4pzh8RX5oWSL3mkeiZTh+vB3h6AzdBcEdQHuSZDvctmsjAnLM1Xj2itoV3ZIebenFH3RXOew4CpmqiI8FP3rfuWG4x9XjXh9oxgG6ojImsJSxsMjT8efhpqYuvOF5YsxG5EsQK8nnkt2Lfs6Bxf01klwDfRpJENFF3R32C4fSUmsVKeARzgtFf1+U==rqA8\" }")	
+        # signature has data and sign.
+#        saveflag("{\n\"signer\" : \"james010kim\", \"newflag\" : \"thisisnewflag\", \"signature\" : \"owEBVgGp/pANAwACAfCPYHY8FuWzAawmYgdtc2cudHh0WR0C4GphbWVzMDEwa2ltOnRoaXNpc25ld2ZsYWeJARwEAAECAAYFAlkdAuAACgkQ8I9gdjwW5bPBEAgAsSGaZtJ03XxVcTgKlLBu+LRymtMRApc6I0I5e0H/9/5WDkLrPsUJ8G72fFu2oQ97sl/V9hUeT90xgqSHFgfZuxRanybTGbSNiJ1g56RY1B7Yszf+1kutgOZnab4xNAeadadzNOD2KYlrwfQFNVT2cVl4679oG17XRiQLJOIR+gAKIvjE0O/U+P8tQ6Yhe5mzhx9dbwawV7nUW9fvfQNQhPMR7F8Ek0wHkBwVfG2qbRjLuuAULA21BHHxQijyjpOdaTX9bhPURBQ20hHPfy1oWxwGHNbW17QoNV53tWHeL3UZ33g3/5iFOB1uuXGGTmpkOGd5wMC8b6RMR9qdC3lKjw==\" }")	
     except KeyboardInterrupt:
         print ("Ctrl_C pressed ... Shutting Down")
