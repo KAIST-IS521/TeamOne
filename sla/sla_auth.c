c5093d6c1e35c895968c54912adc1dcd6c61e230
JaeminPark <jmpark81@kaist.ac.kr>
2017-05-23 09:47:59 +0900

    Add a routine to enter the registration menu

--- sla/sla_auth.c
+++ sla/sla_auth.c
@@ -46,7 +46,8 @@ void hexdump(const void* data, size_t size) {
 }
 
 int main(int argc, char *argv[]) {
-    int cli_fd, len;
+    int cli_fd, len, i;
+    int is_received;
 
     if (argc != 3){
         printf("Usage: %s <ip> <port>\n", argv[0]);
@@ -68,14 +69,41 @@ int main(int argc, char *argv[]) {
         exit(1);
     }
 
-    /* get the msg from the server */
+    /* get the initial msg from the server */
+    // FIXME - replace with recvMsgUntil()
+    printf("1. Receiving the initial msg from the server...\n");
+    is_received = 0;
     while((len = read(cli_fd, buf, sizeof(buf)-1)) > 0){
         setbuf(stdout, NULL);
         buf[len] = 0;
         if(fputs(buf, stdout) == EOF){
             printf("\n Error: fputs error\n");
         }
+        for(i=0;i<len;i++){
+          if(buf[i] == '>') {
+              is_received = 1;
+              break;
+          } // if '>' go next
+        }
+
+        if(is_received == 1) break;
     }
+
+    /* send the menu for registration */
+    // FIXME - replace with sendMsg()
+    printf("2. Sending the selection to the server...\n");
+    write(cli_fd, "2\n", strlen("2\n"));
+
+    /* get the msg from the server */
+    // FIXME - replace with recvMsgUntil()
+     while((len = read(cli_fd, buf, sizeof(buf)-1)) > 0){
+        setbuf(stdout, NULL);
+        buf[len] = 0;
+        if(fputs(buf, stdout) == EOF){
+            printf("\n Error: fputs error\n");
+        }
+    }
+   
 
     closeSock(cli_fd);
 
