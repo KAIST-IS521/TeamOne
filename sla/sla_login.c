e89ce75a8c170367d4e1a701b5165eec234b19a9
Jangha Kim <james010kim@gmail.com>
2017-05-23 13:58:07 +0900

    Modify return value and remove printf(buf)

--- sla/sla_login.c
+++ sla/sla_login.c
@@ -51,11 +51,11 @@ int recvMsgUntilBK(int cli_fd) {
     int is_received = 0;
 
     while((len = read(cli_fd, buf, sizeof(buf)-1)) > 0){
-        setbuf(stdout, NULL);
+//        setbuf(stdout, NULL);
         buf[len] = 0;
-        if(fputs(buf, stdout) == EOF){
-            printf("\n Error: fputs error\n");
-        }
+//        if(fputs(buf, stdout) == EOF){
+//           printf("\n Error: fputs error\n");
+//        }
         for(i=0;i<len;i++){
           if(buf[i] == '>') {
               is_received = 1;
@@ -92,7 +92,7 @@ int main(int argc, char *argv[]) {
     cli_fd = openTCPSock(ip, port);
 
     if(cli_fd <0){
-        printf("Failed to open socket (rv=%d)\n", cli_fd);
+        printf("Failed to open socket (rv=%d) return 2\n", cli_fd);
         exit(2);		// SLA return 2 : cannot connect
     }
 
@@ -103,46 +103,52 @@ int main(int argc, char *argv[]) {
 
     /* send the menu for registration */
     // FIXME - replace with sendMsg()
-    printf("1. Sending the selection to the server...\n");
-    sendMsg(cli_fd, "1\n");	// 1. login
+    printf("2. Sending the selection to the server(login)...\n");
+    if ( sendMsg(cli_fd, "1\n") == -1 ) return 1;	// 1. login
 
     /* get the msg from the server */
     // FIXME - replace with recvMsgUntil()
-    recvMsgUntilBK(cli_fd);
+    if ( recvMsgUntilBK(cli_fd) == -1 ) return 1;
 
-    if ( sendMsg(cli_fd, "user1\n") == -1 ) return -1;
-    if ( recvMsgUntilBK(cli_fd) == -1 ) return -1;
-    if ( sendMsg(cli_fd, "pass1\n") == -1 ) return -1;
-    if ( recvMsgUntilBK(cli_fd) == -1 ) return -1;
-    if ( sendMsg(cli_fd, "3\n") == -1 ) return -1; // Transfer
-    if ( recvMsgUntilBK(cli_fd) == -1 ) return -1;
-    if ( sendMsg(cli_fd, "admin\n") == -1 ) return -1;	// Receiver's id
-    if ( recvMsgUntilBK(cli_fd) == -1 ) return -1;
-    if ( sendMsg(cli_fd, "0\n") == -1 ) return -1; // Transfer
-    if ( recvMsgUntilBK(cli_fd) == -1 ) return -1;
-    if ( sendMsg(cli_fd, "test\n") == -1 ) return -1; // Transfer
-    if ( recvMsgUntilBK(cli_fd) == -1 ) return -1;
-    if ( sendMsg(cli_fd, "y\n") == -1 ) return -1; // Transfer
-    if ( recvMsgUntilBK(cli_fd) == -1 ) return -1;
-    if ( sendMsg(cli_fd, "\n") == -1 ) return -1; // Back to main menu
-    if ( recvMsgUntilBK(cli_fd) == -1 ) return -1;
+    if ( sendMsg(cli_fd, "user1\n") == -1 ) return 1;
+    if ( recvMsgUntilBK(cli_fd) == -1 ) return 1;
+    if ( sendMsg(cli_fd, "pass1\n") == -1 ) return 1;
+    if ( recvMsgUntilBK(cli_fd) == -1 ) return 1;
+
+    printf("3. Transfer function check\n");
+    if ( sendMsg(cli_fd, "3\n") == -1 ) return 1; // Transfer
+    if ( recvMsgUntilBK(cli_fd) == -1 ) return 1;
+    if ( sendMsg(cli_fd, "admin\n") == -1 ) return 1;	// Receiver's id
+    if ( recvMsgUntilBK(cli_fd) == -1 ) return 1;
+    if ( sendMsg(cli_fd, "0\n") == -1 ) return 1; // Transfer
+    if ( recvMsgUntilBK(cli_fd) == -1 ) return 1;
+    if ( sendMsg(cli_fd, "test\n") == -1 ) return 1; // Transfer
+    if ( recvMsgUntilBK(cli_fd) == -1 ) return 1;
+    if ( sendMsg(cli_fd, "y\n") == -1 ) return 1; // Transfer
+    if ( recvMsgUntilBK(cli_fd) == -1 ) return 1;
+    if ( sendMsg(cli_fd, "\n") == -1 ) return 1; // Back to main menu
+    if ( recvMsgUntilBK(cli_fd) == -1 ) return 1;
    
-    if ( sendMsg(cli_fd, "2\n") == -1 ) return -1; // Transaction history
-    if ( recvMsgUntilBK(cli_fd) == -1 ) return -1;
-    if ( sendMsg(cli_fd, "\n") == -1 ) return -1; // Back to main menu
-    if ( recvMsgUntilBK(cli_fd) == -1 ) return -1;
+    printf("4. Transaction history function check\n");
+    if ( sendMsg(cli_fd, "2\n") == -1 ) return 1; // Transaction history
+    if ( recvMsgUntilBK(cli_fd) == -1 ) return 1;
+    if ( sendMsg(cli_fd, "\n") == -1 ) return 1; // Back to main menu
+    if ( recvMsgUntilBK(cli_fd) == -1 ) return 1;
 
-    if ( sendMsg(cli_fd, "1\n") == -1 ) return -1; // Balance
-    if ( recvMsgUntilBK(cli_fd) == -1 ) return -1;
-    if ( sendMsg(cli_fd, "\n") == -1 ) return -1; // Back to main menu
-    if ( recvMsgUntilBK(cli_fd) == -1 ) return -1;
+    printf("5. Balance check function check\n");
+    if ( sendMsg(cli_fd, "1\n") == -1 ) return 1; // Balance
+    if ( recvMsgUntilBK(cli_fd) == -1 ) return 1;
+    if ( sendMsg(cli_fd, "\n") == -1 ) return 1; // Back to main menu
+    if ( recvMsgUntilBK(cli_fd) == -1 ) return 1;
 
-    if ( sendMsg(cli_fd, "5\n") == -1 ) return -1; // Logout
-    if ( recvMsgUntilBK(cli_fd) == -1 ) return -1;
-    if ( sendMsg(cli_fd, "3\n") == -1 ) return -1; // Terminate
-    if ( recvMsgUntilBK(cli_fd) == -1 ) return -1;
+    printf("6. Logout function check\n");
+    if ( sendMsg(cli_fd, "5\n") == -1 ) return 1; // Logout
+    if ( recvMsgUntilBK(cli_fd) == -1 ) return 1;
+    if ( sendMsg(cli_fd, "3\n") == -1 ) return 1; // Terminate
+    if ( recvMsgUntilBK(cli_fd) == -1 ) return 1;
 
     closeSock(cli_fd);
+    printf("Done. return 0\n");
 
     return 0;		// SLA check 0 : normal
 }
