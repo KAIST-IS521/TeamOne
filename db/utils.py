1551de6989c4f39e76e6ead0af131cccf9fb6fd4
ChiHyun Song <ian0371@gmail.com>
2017-05-24 20:26:08 +0900

    Generate MySQL root password randomly

--- db/utils.py
+++ db/utils.py
@@ -3,7 +3,7 @@ import pymysql.cursors
 class bankDB:
     def __init__(self, host='localhost',
                        user='root',
-                       password='DtWb3q2WcG0tc2fFgXi5sGx86XL7RT8g',
+                       password = open("root.pw","rt").read(),
                        db='bankDB'):
         '''
             localhost, root, ~
@@ -230,3 +230,12 @@ class bankDB:
     # Disconnect
     def close_conn(self):
         self.conn.close()
+
+def gen_rand_pw(N):
+    import os
+    import random
+    import string
+    random.seed(os.urandom(64))
+    return ''.join(random.choice(string.ascii_uppercase + \
+                   string.ascii_lowercase + string.digits) \
+                   for _ in range(N))
