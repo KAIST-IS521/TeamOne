ad11c4e223c17a3d8c30b3cb8ee632c9782913ec
Jangha Kim <james010kim@gmail.com>
2017-05-24 20:19:17 +0900

    Modify recv_line for precessing unexpected close

--- server/util.py
+++ server/util.py
@@ -1,24 +1,33 @@
 from .const import *
 
 def print_logo(conn):
-    conn.send(CLEAR_TERMINAL + COR_LOGO) # clear terminal & colored
-    conn.sendall(b"  /$$$$$$$   /$$$$$$  /$$   /$$ /$$   /$$\n" +
-                 b" | $$__  $$ /$$__  $$| $$$ | $$| $$  /$$/\n" + 
-                 b" | $$  \ $$| $$  \ $$| $$$$| $$| $$ /$$/ \n" +
-                 b" | $$$$$$$ | $$$$$$$$| $$ $$ $$| $$$$$/  \n" +
-                 b" | $$__  $$| $$__  $$| $$  $$$$| $$  $$  \n" +
-                 b" | $$  \ $$| $$  | $$| $$\  $$$| $$\  $$ \n" +
-                 b" | $$$$$$$/| $$  | $$| $$ \  $$| $$ \  $$\n" +
-                 b" |_______/ |__/  |__/|__/  \__/|__/  \__/\n\n")
+    try:
+        conn.send(CLEAR_TERMINAL + COR_LOGO) # clear terminal & colored
+        conn.sendall(b"  /$$$$$$$   /$$$$$$  /$$   /$$ /$$   /$$\n" +
+                     b" | $$__  $$ /$$__  $$| $$$ | $$| $$  /$$/\n" +
+                     b" | $$  \ $$| $$  \ $$| $$$$| $$| $$ /$$/ \n" +
+                     b" | $$$$$$$ | $$$$$$$$| $$ $$ $$| $$$$$/  \n" +
+                     b" | $$__  $$| $$__  $$| $$  $$$$| $$  $$  \n" +
+                     b" | $$  \ $$| $$  | $$| $$\  $$$| $$\  $$ \n" +
+                     b" | $$$$$$$/| $$  | $$| $$ \  $$| $$ \  $$\n" +
+                     b" |_______/ |__/  |__/|__/  \__/|__/  \__/\n\n")
 
-    conn.send(COR_BASE) # colored
-    conn.send(b" =========================================\n")
+        conn.send(COR_BASE) # colored
+        conn.send(b" =========================================\n")
+    except:
+        print("print_log exception")
 
 def recv_line(conn):
     # recv 1 byte until get '\n'
     data = []
     while True:
-        byte = conn.recv(1)
+        try:
+            byte = conn.recv(1)
+            # closed socket
+            if byte == b'':
+                break
+        except e:
+            print("socket exeception")
         if byte == b'\n':
             break
         data.append(byte)
