3175bcacfa7df59da25715c836eba3c8f1fbc276
JaeminPark <jmpark81@kaist.ac.kr>
2017-05-27 02:07:49 +0900

    Remove tab in the source

--- sla/sla_auth.c
+++ sla/sla_auth.c
@@ -22,17 +22,17 @@
 #define UNAME "Happyhacking TeamOne"
 #define AUTH_SUCCESS "Username ->"
 
-#define fail_if_err(err)					\
-  do								\
-    {								\
-      if (err)							\
-        {							\
-          fprintf (stderr, "%s:%d: %s: %s\n",			\
-                   __FILE__, __LINE__, gpgme_strsource (err),	\
-		   gpgme_strerror (err));			\
-          exit(1);                                           \
-        }							\
-    }								\
+#define fail_if_err(err	)                                       \
+  do                                                            \
+    {                                                           \
+      if (err)                                                  \
+        {                                                       \
+          fprintf (stderr, "%s:%d: %s: %s\n",                   \
+                   __FILE__, __LINE__, gpgme_strsource (err),   \
+		   gpgme_strerror (err));                       \
+          exit(1);                                              \
+        }                                                       \
+    }                                                           \
   while (0)
 
 /* read gpgme */
