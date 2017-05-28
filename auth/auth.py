9d16aeb837912260f94d01986c891dfa69f1abdf
ChiHyun Song <ian0371@gmail.com>
2017-05-28 15:18:21 +0900

    Remove id_list bug

--- auth/auth.py
+++ auth/auth.py
@@ -13,16 +13,17 @@ def initialize_gpg():
     global gpg
     homedir = expanduser("~") + "/.gnupg"
     try:
-        gpg = gnupg.GPG(gnupghome=homedir) 
+        gpg = gnupg.GPG(gnupghome=homedir)
     except TypeError:
         gpg = gnupg.GPG(homedir=homedir)
 
 # Check if the registered githubId
 def check_registered(github_id):
-    with open("./db/github_id.list") as file:
-         for id in file:
-             id = id.strip()
-             id_list.append(id)
+    if not id_list:
+        with open("./db/github_id.list") as file:
+             for id in file:
+                 id = id.strip()
+                 id_list.append(id)
     return github_id in id_list
 
 # Generate a big random number (256-bit)
@@ -41,8 +42,8 @@ def generate_challenge(github_id, rand, input_passphrase):
     my_pubkey = gpg.import_keys(my_key_data)
 
     # Encrypt the generated random using the imported public key
-    encrypted_data = gpg.encrypt(hex(rand), pubkey.fingerprints[0], 
-                                 default_key=my_pubkey.fingerprints[0], 
+    encrypted_data = gpg.encrypt(hex(rand), pubkey.fingerprints[0],
+                                 default_key=my_pubkey.fingerprints[0],
                                  passphrase=input_passphrase)
     encrypted_string = str(encrypted_data)
 
@@ -51,11 +52,11 @@ def generate_challenge(github_id, rand, input_passphrase):
 
 # Verify the response from the user
 def verify_response(github_id, encrypted_string, input_passphrase):
-    decrypted_data = gpg.decrypt(encrypted_string, 
+    decrypted_data = gpg.decrypt(encrypted_string,
                                  passphrase=input_passphrase)
 
     if(decrypted_data.ok != True):
         print("Failed to decrypt the response!")
-        return  
-     
-    return str(decrypted_data)   
+        return
+
+    return str(decrypted_data)
